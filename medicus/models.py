7c970188082d5ad422c39e0b2713445f723fe8ea
LYK <dalinaum@gmail.com>
2023-05-05 17:09:57 +0000

    영업시간을 넣고 Admin에 TabularInline으로 추가

--- medicus/models.py
+++ medicus/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext as _
+
+WEEKDAYS = [
+    (1, _("Monday")),
+    (2, _("Tuesday")),
+    (3, _("Wednesday")),
+    (4, _("Thursday")),
+    (5, _("Friday")),
+    (6, _("Saturday")),
+    (7, _("Sunday")),
+]
 
 
 class Patient(models.Model):
@@ -20,3 +31,25 @@ class Doctor(models.Model):
     office_name = models.CharField(max_length=50)
     specialties = models.ManyToManyField(MedicalSpecialty)
     non_reimbursable = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.office_name})"
+
+
+class OpeningHour(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    weekday = models.IntegerField(choices=WEEKDAYS)
+    from_hour = models.TimeField()
+    to_hour = models.TimeField()
+    lunch_hour = models.TimeField(null=True, blank=True)
+    lunch_end_hour = models.TimeField(null=True, blank=True)
+
+    def __str__(self):
+        formatted = f"{self.doctor} {self.from_hour} ~ {self.to_hour}"
+        if self.lunch_hour is None:
+            return formatted
+        return f"{formatted} (점심 {self.lunch_hour} ~ {self.lunch_end_hour})"
+
+
+    class Meta:
+        unique_together = ('doctor', 'weekday')
