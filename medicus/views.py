ca83e8b047e6b4ae5356b08aec8efbc6f897e1f8
LYK <dalinaum@gmail.com>
2023-05-06 14:08:08 +0000

    포매팅 수정

--- medicus/views.py
+++ medicus/views.py
@@ -12,4 +12,5 @@ class DoctorList(generics.ListAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSereailizer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['specialties__name', "name", "office_name", "non_reimbursable"]
+    search_fields = ['specialties__name', "name",
+                     "office_name", "non_reimbursable"]
