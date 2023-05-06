4df5281c0dc3db83aa7e4c8dcbd5ca9608a58bbf
LYK <dalinaum@gmail.com>
2023-05-06 14:08:59 +0000

    사용하지 않는 import 제거

--- medicus/views.py
+++ medicus/views.py
@@ -1,11 +1,7 @@
 # from django.shortcuts import render
-from rest_framework import viewsets, filters, generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import filters, generics
 from .serializiers import DoctorSereailizer
 from .models import Doctor
-
-from rest_framework import filters
 
 
 class DoctorList(generics.ListAPIView):
